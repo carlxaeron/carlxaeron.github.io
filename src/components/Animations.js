039e9d47f805d48ce4930a5309bc9510c6b214de
Carl Louis Manuel <carlxaeron09@gmail.com>
2024-02-06 16:41:00 +0800

    +

--- src/components/Animations.js
+++ src/components/Animations.js
@@ -1,13 +1,39 @@
 import { useSpring, animated } from "@react-spring/web";
+import { useTransition } from "react";
 
 function AnimationDown({ children }) {
-    const springs = useSpring({
-        from: { y: -200, opacity: 0, },
-        to: { y: 0, opacity: 1, },
-    })
+    // const tos = [];
+
+    // children.map((v,i) => {
+    //     const delay = (5000 * (i + 1))
+    //     console.log(delay)
+    //     tos.push({ y: 0, opacity: 1, delay })
+    // })
+
+    // const Test = () => {
+    //     const springs = useSpring({
+    //         from: { y: -200, opacity: 0, },
+    //         to: tos,
+    //     })
+
+    //     const newChild = children.map((v,i) => {
+    //         console.log(springs)
+    //         return <animated.div style={{...springs}}>{v}</animated.div>
+    //     })
+
+    //     return newChild;
+    // }
+
+    children.map(v => console.log(v))
+
+    const transitions = useTransition(
+        // children.map()
+    )
 
     return (
-        <animated.div style={{...springs}}>{children}</animated.div>
+        <>
+            {/* <Test/> */}
+        </>
     )
 }
 
--- src/containers/Portfolio/Home.js
+++ src/containers/Portfolio/Home.js
@@ -15,7 +15,7 @@ function PortfolioHome() {
                 </AnimationDown>) }
                 { themeContext.value.env === 'prod' && (
                 <AnimationDown>
-                    <h2>Hello, I'm a <span>Fullstack PHP Developer</span></h2>
+                    <h2>Hello, I'm a <span>Fullstack Web / App Developer</span></h2>
                     <h3>I am a Web Developer who seeks challenging web development job.</h3><a className="btn btn-primary" href="#projects">View my works</a>
                 </AnimationDown>
                 )}
